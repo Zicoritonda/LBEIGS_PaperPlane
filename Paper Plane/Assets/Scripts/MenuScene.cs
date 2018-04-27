4bf0fefd411a62fe3df3f898a49cdcec3944195c
Zico Ritonda <36809968+Zicoritonda@users.noreply.github.com>
2018-04-27 12:16:54 +0700

    new folder

---
+++ Paper Plane/Assets/Scripts/MenuScene.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScene : MonoBehaviour {
+
+    private CanvasGroup fadeGroup;
+    private float fadeInspeed = 0.33f;
+
+    public RectTransform menuContainer;
+    public Transform gamePanel;
+
+    private Vector3 desiredMenuPosition;
+
+    private void Start()
+    {
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+        fadeGroup.alpha = 1;
+
+        InitGamePlay();
+    }
+
+    private void Update()
+    {
+        fadeGroup.alpha = 1 - Time.timeSinceLevelLoad * fadeInspeed;
+
+        //menu navigation
+        menuContainer.anchoredPosition3D = Vector3.Lerp(menuContainer.anchoredPosition3D, desiredMenuPosition, 0.1f);
+    }
+    
+    private void InitGamePlay()
+    {
+        if (gamePanel == null) //memastikan untuk menentukan referensi
+            Debug.Log("Didnt assign game panel in the inspector");
+
+        int i = 0;
+        foreach(Transform t in gamePanel)
+        {
+            int currentIndex = i;
+            Button b = t.GetComponent<Button>();
+            b.onClick.AddListener(() => OnModeSelect(currentIndex));
+
+            i++;
+        }
+    }
+
+    private void NavigateTo(int menuIndex)
+    {
+        switch (menuIndex)
+        {
+            //main menu
+            default:
+            case 0:
+                desiredMenuPosition = Vector3.zero;
+                break;
+            case 1:
+                desiredMenuPosition = Vector3.left * 1280;
+                break;
+        }
+    }
+
+    //button
+    private void OnModeSelect(int currentIndex)
+    {
+        Manager.Instance.currentMode = currentIndex;
+        Debug.Log("selecting current index" + currentIndex);
+    }
+
+    public void OnPlayClick()
+    {
+        NavigateTo(1);
+        Debug.Log("Bisa play");
+    }
+
+    public void OnFreeFlyClick()
+    {
+        SceneManager.LoadScene("Game");
+        Debug.Log("Bisa fly");
+    }
+
+    public void OnChallengeClick()
+    {
+        Debug.Log("Bisa challenge");
+    }
+
+    public void OnPlaneClick()
+    {
+        Debug.Log("Bisa plane");
+    }
+
+    public void OnBackClick()
+    {
+        NavigateTo(0);
+        Debug.Log("bisa back");
+    } 
+}
---
+++ Paper Plane/Assets/Scripts/PlaneControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneControl : MonoBehaviour {
+
+
+    public float speed = 90.0f;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("plane pilot script added to:" + gameObject.name);
+
+        
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 moveCamTo = transform.position - transform.forward * 0.5f + Vector3.up * 2.5f;
+        float bias = 0.96f;
+        Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1.0f - bias);
+        
+        Camera.main.transform.LookAt(transform.position + transform.forward * 0.5f);
+
+        transform.position += transform.forward * Time.deltaTime * 25.0f;
+
+        speed -= transform.forward.y * Time.deltaTime * 12.0f;
+
+        if(speed < 5.0f)
+        {
+            speed = 5.0f;
+        }
+        
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+
+        float terrainHeight = Terrain.activeTerrain.SampleHeight(transform.position);
+
+        if(terrainHeight > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, terrainHeight, transform.position.z);
+        }
+	}
+}
